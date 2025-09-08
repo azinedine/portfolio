b322306d0c4e570cf52b10f143221f3e453ccf68
azineddine <amarichezineddine@gmail.com>
2025-09-08 10:51:06 +0100

    Add index file for ExperiencePage to export components, data, and animations, improving organization and accessibility.

---
+++ src/app/experience/index.ts
@@ -0,0 +1,7 @@
+// Export all components and data
+export { default as ExperiencePage } from './page';
+export { default as TimelineItem } from './TimelineItem';
+export { default as CertificationCard } from './CertificationCard';
+export { experiences, education, certifications } from './data';
+export type { ExperienceItem, Certification } from './data';
+export { containerVariants, itemVariants, slideInLeft, slideInRight, scaleIn } from './animations';
