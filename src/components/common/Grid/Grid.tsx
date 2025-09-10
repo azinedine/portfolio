06af977d672b38bcd5146b45269f14d1855885e1
azineddine <amarichezineddine@gmail.com>
2025-09-11 00:48:41 +0100

    Refactor Grid component for improved responsiveness: update grid column classes to utilize 'sm' breakpoints, enhancing layout adaptability across various screen sizes.

--- src/components/common/Grid/Grid.tsx
+++ src/components/common/Grid/Grid.tsx
@@ -48,11 +48,11 @@ const getGridCols = (cols: number, responsive: boolean) => {
   
   const responsiveClasses = {
     1: 'grid-cols-1',
-    2: 'grid-cols-1 lg:grid-cols-2',
-    3: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3',
-    4: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4',
-    5: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5',
-    6: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6',
+    2: 'grid-cols-1 sm:grid-cols-2',
+    3: 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3',
+    4: 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4',
+    5: 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5',
+    6: 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6',
   }
   
   return responsiveClasses[cols as keyof typeof responsiveClasses] || responsiveClasses[2]
